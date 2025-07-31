4e0b5451cb7866e849e1a28f3c276e48db381469
Secretor007 <prathamkadave18@gmail.com>
2025-07-31 23:34:45 +0530

    Added JWT Functionality

--- src/main/java/com/example/demo/ProjectApplication.java
+++ src/main/java/com/example/demo/ProjectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example")
@@ -13,6 +14,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class ProjectApplication {
 
 	public static void main(String[] args) {
+		String rawPass = "Ezio";
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String hashedPassword = encoder.encode(rawPass);
+		System.out.println("Hashed Password: " + hashedPassword);
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
