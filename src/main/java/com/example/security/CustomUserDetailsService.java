4d899ec5b7e7def98c4ee9952dbf70d5b0e0c93b
Secretor007 <prathamkadave18@gmail.com>
2025-08-01 11:38:09 +0530

    Added AOP and Lombok

--- src/main/java/com/example/security/CustomUserDetailsService.java
+++ src/main/java/com/example/security/CustomUserDetailsService.java
@@ -1,9 +1,6 @@
 package com.example.security;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
